28bf4011f8135b90609f026a29bf8bd19cc0d346
Daniel Minto <mintodani@myvuw.ac.nz>
2025-05-18 20:14:32 +1200

    Written a draft of the Interface that will be the "basis" for the Control Signals.

--- src/main/java/fixit_pioneers/ControlSignals/ControlSignals.java
+++ src/main/java/fixit_pioneers/ControlSignals/ControlSignals.java
@@ -1,5 +1,19 @@
 package fixit_pioneers.ControlSignals;
 
-public class ControlSignals {
-    
+public interface ControlSignals {
+    double speed = 0.0;
+    double brakePower = 0.75;
+
+    public void processSensorData(SensorType sensor, double power);
+    public void processUserInput(Action action, double power);
+
+    default void triggerAntiBrakeLock(){}
+
+    public void makeAnAction();
 }
+
+/**
+ * "Mock Objects" to make the code compile.
+ */
+record SensorType(){}
+record Action(){}

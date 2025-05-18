cd63f084923d5c4c0861838fe007c5f23aa147df
Daniel Minto <mintodani@myvuw.ac.nz>
2025-05-18 20:15:15 +1200

    Started writing comments. These will need finishing.

--- src/main/java/fixit_pioneers/ControlSignals/ControlSignals.java
+++ src/main/java/fixit_pioneers/ControlSignals/ControlSignals.java
@@ -4,9 +4,23 @@ public interface ControlSignals {
     double speed = 0.0;
     double brakePower = 0.75;
 
+    /**
+     *
+     * @param sensor
+     * @param power
+     */
     public void processSensorData(SensorType sensor, double power);
+
+    /**
+     *
+     * @param action
+     * @param power
+     */
     public void processUserInput(Action action, double power);
 
+    /**
+     * To prevent the wheels from locking up during braking, the Anti-Lock Brake system will activate automatically.
+     */
     default void triggerAntiBrakeLock(){}
 
     public void makeAnAction();

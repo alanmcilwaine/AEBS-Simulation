7eaaa4948b62a7fbd0ae066e02a1b991b55def23
Daniel Minto <mintodani@myvuw.ac.nz>
2025-05-18 22:49:43 +1200

    Have finished "rough" scaffold of "ControlSignals" Interface.

--- src/main/java/fixit_pioneers/ControlSignals/ControlSignals.java
+++ src/main/java/fixit_pioneers/ControlSignals/ControlSignals.java
@@ -1,8 +1,11 @@
 package fixit_pioneers.ControlSignals;
 
 public interface ControlSignals {
+    /** The current speed of the car. */
     double speed = 0.0;
-    double brakePower = 0.75;
+
+    /** The current power of the brakes being applied. */
+    double brakePower = 0.0;
 
     /**
      * Takes a given car sensor, and processes the data in it.
@@ -10,7 +13,7 @@ public interface ControlSignals {
      * @param sensor The car sensor of interest, that holds the data.
      * @param power The brake power to apply to the car.
      */
-    public void processSensorData(SensorType sensor, double power);
+    void processSensorData(SensorType sensor, double power);
 
     /**
      * Processes user input.
@@ -18,18 +21,18 @@ public interface ControlSignals {
      * @param action The action that the car brakes will take.
      * @param power The brake power to apply to the car.
      */
-    public void processUserInput(Action action, double power);
+    void processUserInput(Action action, double power);
 
     /**
-     * To prevent the wheels from locking up during braking, the Anti-Lock Brake system will activate automatically.
+     * To prevent the wheels from locking up during braking, the Anti-Lock
+     * Brake system will activate automatically.
      */
-    default void triggerAntiBrakeLock(){}
+    default void triggerAntiBrakeLock() {}
 
-    public void makeAnAction();
+    /** Make an action! (Not sure what it does at this stage.) */
+    void makeAnAction();
 }
 
-/**
- * "Mock Objects" to make the code compile.
- */
-record SensorType(){}
-record Action(){}
+/** "Mock Objects" to make the code compile. */
+record SensorType() {}
+record Action() {}

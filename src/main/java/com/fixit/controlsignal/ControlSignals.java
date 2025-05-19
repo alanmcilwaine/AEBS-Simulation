964fc0d9c91636695cce6a84bfdd46ffa352856d
Daniel Minto <mintodani@myvuw.ac.nz>
2025-05-20 11:57:09 +1200

    Made an interface for the Control Signals, in case there will be more than one type.

--- src/main/java/com/fixit/controlsignal/ControlSignals.java
+++ src/main/java/com/fixit/controlsignal/ControlSignals.java
@@ -1,36 +1,23 @@
 package com.fixit.controlsignal;
 
-public interface ControlSignals {
+public class ControlSignals implements ControlSignal{
     /** The current speed of the car. */
     double speed = 0.0;
 
     /** The current power of the brakes being applied. */
     double brakePower = 0.0;
 
-    /**
-     * Takes a given car sensor, and processes the data in it.
-     *
-     * @param sensor The car sensor of interest, that holds the data.
-     * @param power The brake power to apply to the car.
-     */
-    void processSensorData(SensorType sensor, double power);
+    public void processSensorData(SensorType sensor, double power){
 
-    /**
-     * Processes user input.
-     *
-     * @param action The action that the car brakes will take.
-     * @param power The brake power to apply to the car.
-     */
-    void processUserInput(Action action, double power);
+    }
 
-    /**
-     * To prevent the wheels from locking up during braking, the Anti-Lock
-     * Brake system will activate automatically.
-     */
-    default void triggerAntiBrakeLock() {}
+    public void processUserInput(Action action, double power){
 
-    /** Make an action! (Not sure what it does at this stage.) */
-    void makeAnAction();
+    }
+
+    public void makeAnAction(){
+
+    }
 }
 
 /** "Mock Objects" to make the code compile. */

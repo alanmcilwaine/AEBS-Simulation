757a96669a8ead361a5c90e556127ed6bd39de79
Ming Bao <mingbao2891@gmail.com>
2025-05-19 12:50:41 +1200

    added fail tests

--- src/test/java/fixit_pioneers/Simulation/SimulationTest.java
+++ src/test/java/fixit_pioneers/Simulation/SimulationTest.java
@@ -8,4 +8,14 @@ public class SimulationTest {
     public void test1(){
         assert true;
     }
+
+    @Test
+    public void test2(){
+        assert false : "Pioneer: inadequate";
+    }
+
+    @Test
+    public void test3(){
+        assert false : "Pioneer: very inadequate";
+    }
 }

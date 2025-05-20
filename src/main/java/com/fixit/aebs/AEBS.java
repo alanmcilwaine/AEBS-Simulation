c8c968da1c71bcc6f2b453446b8c0b05ff2a92a6
DoritoClod95 <carlaparinas1231@gmail.com>
2025-05-20 12:59:30 +1200

    add general skeleton body for AEBS interface

--- src/main/java/com/fixit/aebs/AEBS.java
+++ src/main/java/com/fixit/aebs/AEBS.java
@@ -1,5 +1,23 @@
 package com.fixit.aebs;
 
-public class AEBS {
-    
+public interface AEBS {
+
+    /**
+     * Evaluate the distance between the vehicle and other objects in the path and
+     * determine whether they need braking.
+     * @param distanceData the distance of objects from the car
+     * @param wheelSpeed the speed of the wheel
+     * @return returns how much brake to apply to the car
+     */
+     double evaluateBraking(double distanceData, double wheelSpeed);
+
+    /**
+     * Passes in the vehicle's wheel speed and that calculates the threshold on
+     * when the brakes should hit
+     * @param wheelSpeed the speed of the wheel
+     * @return the braking threshold
+     */
+    double determineThreshold(double wheelSpeed);
+
+
 }

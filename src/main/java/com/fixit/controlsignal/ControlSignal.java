610d069eef1751e7267269bdc61ff74dc36aba79
Daniel Minto <mintodani@myvuw.ac.nz>
2025-05-20 12:05:35 +1200

    Fixed Checkstyle issues in ControlSignal.java

--- src/main/java/com/fixit/controlsignal/ControlSignal.java
+++ src/main/java/com/fixit/controlsignal/ControlSignal.java
@@ -1,29 +1,29 @@
 package com.fixit.controlsignal;
 
-interface ControlSignal{
+interface ControlSignal {
+  /**
+   * Takes a given car sensor, and processes the data in it.
+   *
+   * @param sensor The car sensor of interest, that holds the data.
+   * @param power The brake power to apply to the car.
+   */
+  void processSensorData(SensorType sensor, double power);
 
-    /**
-     * Takes a given car sensor, and processes the data in it.
-     *
-     * @param sensor The car sensor of interest, that holds the data.
-     * @param power The brake power to apply to the car.
-     */
-    void processSensorData(SensorType sensor, double power);
+  /**
+   * Processes user input.
+   *
+   * @param action The action that the car brakes will take.
+   * @param power The brake power to apply to the car.
+   */
+  void processUserInput(Action action, double power);
 
-    /**
-     * Processes user input.
-     *
-     * @param action The action that the car brakes will take.
-     * @param power The brake power to apply to the car.
-     */
-    void processUserInput(Action action, double power);
+  /**
+   * To prevent the wheels from locking up during braking, the Anti-Lock
+   * Brake system will activate automatically.
+   */
+  default void triggerAntiBrakeLock() {
+  }
 
-    /**
-     * To prevent the wheels from locking up during braking, the Anti-Lock
-     * Brake system will activate automatically.
-     */
-    default void triggerAntiBrakeLock() {}
-
-    /** Make an action! (Not sure what it does at this stage.) */
-    void makeAnAction();
+  /** Make an action! (Not sure what it does at this stage). */
+  void makeAnAction();
 }

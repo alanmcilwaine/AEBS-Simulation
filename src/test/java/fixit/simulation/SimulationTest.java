5a3d638e4c52c3da6237feda30a2f9cb8c5e4bef
Ming Bao <mingbao2891@gmail.com>
2025-05-21 00:31:41 +1200

    added test tests

---
+++ src/test/java/fixit/simulation/SimulationTest.java
@@ -0,0 +1,20 @@
+package fixit.simulation;
+
+import org.junit.Test;
+
+public class SimulationTest {
+    @Test
+    public void test1() {
+        assert true;
+    }
+
+    @Test
+    public void test2() {
+        assert false : "Pioneer: inadequite";
+    }
+
+    @Test
+    public void test3() {
+        assert false : "Pioneer: very inadequite";
+    }
+}

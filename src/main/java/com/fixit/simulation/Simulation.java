9eef6bc18ae96f516b09bb927fb50e4cd3f5e2a9
Ming Bao <mingbao2891@gmail.com>
2025-05-21 17:54:37 +1200

    add pedantic compiler flags

--- src/main/java/com/fixit/simulation/Simulation.java
+++ src/main/java/com/fixit/simulation/Simulation.java
@@ -4,8 +4,10 @@ import com.fixit.car.Car;
 import com.fixit.car.sensors.SensorType;
 
 public class Simulation {
-    public static void main(String[] args) {
-        Car car = Car.instance();
-        car.sensorInput(null, 6.1);
-    }
+  public static void main(String[] args) {
+    Car car = Car.instance();
+    car.sensorInput(SensorType.LIDARCENTRE, 6.1);
+
+    Car c = Car.instance();
+  }
 }

23b5b505a2248f281350ae008b69c9ad91401656
Daniel Minto <mintodani@myvuw.ac.nz>
2025-05-21 17:07:43 +1200

    Added in more detail to the Anti Brake System after class diagram update.

--- src/main/java/com/fixit/controlsignal/AntiBrakeSystem.java
+++ src/main/java/com/fixit/controlsignal/AntiBrakeSystem.java
@@ -1,5 +1,17 @@
 package com.fixit.controlsignal;
 
+/**
+ * The Anti-Lock Brake system prevents the wheels from locking up during
+ * braking. This will occur if there is a sudden change in the wheel-speed.
+ * (Determined by the Wheel-speed's deceleration.)
+ */
 public class AntiBrakeSystem {
+  int wheelSpeed;
+  int brakePower;
 
+  /**
+   *
+   */
+  public void evaluate(int brakePower, int wheelSpeed){
+  }
 }

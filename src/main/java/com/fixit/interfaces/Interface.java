47ad628de031e1bc50edf2945614b4aef4bbf04d
Luigi Ibanez <ibanezluig@ecs.vuw.ac.nz>
2025-05-21 18:13:35 +1200

    created JavaDoc for Interface and UI

--- src/main/java/com/fixit/interfaces/Interface.java
+++ src/main/java/com/fixit/interfaces/Interface.java
@@ -1,5 +1,32 @@
 package com.fixit.interfaces;
 
+/**
+ * Public API access to Interface (UI)
+ *
+ */
 public interface Interface {
+  /**
+   * autoSystemToggle method calls controlSignal to
+   * enable or disable the Automatic Emergency Braking system.
+   * Returns void, but tells the user the state
+   * of the system (on or off)
+   */
+  void autoSystemToggle();
 
+  /**
+   * applyBrake method calls controlSignal to apply the amount
+   * of brakes specified by the driver to slow down.
+   * Returns void.
+   *
+   * @param brakePower The amount of power to send to the brakes.
+   */
+  void applyBrake(double brakePower);
+  /**
+   * applyThrottle method calls controlSignal to apply the amount
+   * of throttle specified by the driver to speed up.
+   * Returns void.
+   *
+   * @param throttlePower the amount of throttle to send to car.
+   */
+  void applyThrottle(double throttlePower);
 }
--- src/main/java/com/fixit/interfaces/UI.java
+++ src/main/java/com/fixit/interfaces/UI.java
@@ -1,5 +1,29 @@
 package com.fixit.interfaces;
 
+/**
+ * UI class holds functionality with the UI.
+ * Contains methods to trigger brakes, and disable the
+ * Automatic Braking system
+ */
 public class UI implements Interface {
-    
+  @Override
+  public void autoSystemToggle() {
+    //call CS to toggle Automatic Emergency Brake System
+  }
+  /**
+   * applyBrake method calls controlSignal to apply the amount
+   * of brakes specified by the driver.
+   * Returns void
+   *
+   * @param brakePower The amount of power to send to the brakes.
+   */
+  @Override
+  public void applyBrake(double brakePower){
+    //call CS to apply brakes
+  }
+
+  @Override
+  public void applyThrottle(double throttlePower) {
+
+  }
 }

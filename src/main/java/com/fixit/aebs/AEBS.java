9579c4f3e8cf05cee73d01c94ed8e7f9d801c718
Carla Parinas <parinamari@ecs.vuw.ac.nz>
2025-05-22 13:48:40 +1200

    added a way for control signal to get data from aebs

--- src/main/java/com/fixit/aebs/AEBS.java
+++ src/main/java/com/fixit/aebs/AEBS.java
@@ -7,24 +7,38 @@ package com.fixit.aebs;
 public class AEBS implements AutoBrakeSystem {
 
   /**
+   * the amount of brakes to send to control signal.
+   */
+  private double brakeValue = 0;
+
+  /**
+   * Returns the amount of brakes needed for the car.
+   * Gives a 0 if no brake should be applied.
+   *
+   * @return returns the amount of brake value needed for the car
+   */
+  @Override
+  public double getBrakeValue() {
+    return brakeValue;
+  }
+
+  /**
    * Evaluates the system's automatic braking system.
    * Based on the distance between the vehicle and the road hazards.
    *
    * @param distanceData the distance of objects from the car
    * @param wheelSpeed the speed of the wheel.
-   * @return returns the braking value.
    */
   @Override
-  public double evaluateBraking(final double distanceData, final double wheelSpeed) {
+  public void evaluateBraking(final double distanceData, final double wheelSpeed) {
     // TODO: Check for any more parameters needed to be added
 
     // Determines whether or braking should happen or not
     // This is to save resources on the calculations
     if (distanceData >= determineThreshold(wheelSpeed)) {
-      return 1;
+      brakeValue = 1;
     }
-
-    return 0;
+    brakeValue = 0;
   }
 
   /**

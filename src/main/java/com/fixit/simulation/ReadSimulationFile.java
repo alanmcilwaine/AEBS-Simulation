23a482867230f128499ad25cd123c6ddf0d3f0ce
Ming Bao <mingbao2891@gmail.com>
2025-05-22 13:20:37 +1200

    added javadoc and getters for readSimulationData

--- src/main/java/com/fixit/simulation/ReadSimulationFile.java
+++ src/main/java/com/fixit/simulation/ReadSimulationFile.java
@@ -8,18 +8,28 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Reads a simulation datafile and
+ * Reads a simulation datafile and store it.
  *
+ * @author Ming Bao
  */
 public class ReadSimulationFile {
   private List<List<Double>> sensorData;
   private String weather;
   private Path filePath;
 
+  /**
+   * Constructs a ReadSimulationFile object with the path to a datafile.
+   *
+   * @param filePath The path of the datafile
+   */
   public ReadSimulationFile(String filePath) {
     this.filePath = Paths.get(filePath);
   }
 
+  /**
+   * Reads the data in the file and store the weather and sensor data
+   * in their respective variables.
+   */
   public void readData() {
     try {
       List<String> content = Files.readAllLines(filePath);
@@ -36,5 +46,23 @@ public class ReadSimulationFile {
     } catch (IOException e) {
       throw new Error("Error reading file");
     }
+  }
+
+  /**
+   * Getter for weather variable
+   *
+   * @return weather
+   */
+  public String weather() {
+    return this.weather;
+  }
+
+  /**
+   * Getter for sensorData variable
+   *
+   * @return sensorData
+   */
+  public List<List<Double>> sensorData() {
+    return this.sensorData;
   }
 }

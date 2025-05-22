8b833d81a7c341003ccef3b98c33e23888433aec
Ming Bao <mingbao2891@gmail.com>
2025-05-22 13:36:00 +1200

    finished simulation

--- src/main/java/com/fixit/simulation/Simulation.java
+++ src/main/java/com/fixit/simulation/Simulation.java
@@ -2,12 +2,25 @@ package com.fixit.simulation;
 
 import com.fixit.car.Car;
 import com.fixit.car.sensors.SensorType;
+import java.util.List;
+import java.lang.Thread;
 
 public class Simulation {
   public static void main(String[] args) {
     ReadSimulationFile file = new ReadSimulationFile("src/main/java/com/fixit/simulation/testdata.txt");
     file.readData();
-
     Car car = Car.instance();
+
+    try {
+      for (List<Double> data : file.sensorData()) {
+        car.sensorInput(SensorType.LIDARLEFT, data.get(0));
+        car.sensorInput(SensorType.LIDARCENTRE, data.get(1));
+        car.sensorInput(SensorType.LIDARRIGHT, data.get(2));
+
+        Thread.sleep(50);
+      }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
   }
 }

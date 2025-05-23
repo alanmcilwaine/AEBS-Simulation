236296b00317683d1b74189f957cf3c8026fbb61
Luigi Ibanez <ibanezluig@ecs.vuw.ac.nz>
2025-05-23 18:09:43 +1200

    made interface thingies static for access

--- src/main/java/com/fixit/interfaces/Interface.java
+++ src/main/java/com/fixit/interfaces/Interface.java
@@ -6,21 +6,27 @@ package com.fixit.interfaces;
  */
 public interface Interface {
   /**
-   * receiveSpeed method gets called by sensor
-   * package and then displays speed to the user.
+   * receiveSpeed method gets called by sensor package
+   * and then displays speed to the user.
    *
-   * @param speed is the unit of measurement (kph) that will be displayed to the user.
+   * @param speed is the unit of measurement (kph)
+   * that will be displayed to the user.
    */
-  void receiveSpeed(double speed);
+  static void receiveSpeed(double speed) {
+
+  }
 
   /**
-   * receiveWarning method gets called by ControlSignal package
-   * when there is a warning that needs to be displayed to the driver.
-   * This can be anything such as ABS being triggered, AutoBrakeSystem
-   * being triggered, or enabled/disabled.
+   * receiveWarning method gets called by ControlSignal
+   * package when there is a warning that needs to be
+   * displayed to the driver. This can be anything such
+   * as ABS being triggered, AutoBrakeSystem being triggered, or
+   * enabled/disabled.
    *
-   * @param warning is the warning object that gets passed from
-   * ControlSignal that gets displayed to the user.
+   * @param warning is the warning object that gets passed from ControlSignal that gets displayed to the user.
    */
-  void receiveWarning(String warning);  //Object type
+  static void receiveWarning(String warning)  //Object type
+  {
+
+  }
 }

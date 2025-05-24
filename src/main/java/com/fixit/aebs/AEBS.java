a19f9be71c7f23ccf7f042b0a5beb826a935e3d4
Carla Parinas <parinamari@ecs.vuw.ac.nz>
2025-05-24 16:19:43 +1200

    implemented singleton format

--- src/main/java/com/fixit/aebs/AEBS.java
+++ src/main/java/com/fixit/aebs/AEBS.java
@@ -6,18 +6,18 @@ package com.fixit.aebs;
  **/
 final class AEBS implements AutoBrakeSystem {
 
-  /* the amount of brakes to send to control signal. */
+  /** the amount of brakes to send to control signal. */
   private static double brakeValue = 0;
 
-  /* checks if the system has received distance and speed values from sensor */
+  /** checks if the system has received distance and speed values from sensor. */
   private static boolean distanceReceived = false;
   private static boolean speedReceived = false;
 
-  /* the distance and speed values themselves that were received */
-  private static double distance = 0;
-  private static double speed = 0;
+  /** the distance and speed values themselves that were received. */
+  private static double distanceData = 0;
+  private static double wheelSpeed = 0;
 
-  /* the singular instance of AEBS that the car is using */
+  /** the singular instance of AEBS that the car is using. */
   private static final AEBS instance = new AEBS();
 
   /**
@@ -35,17 +35,17 @@ final class AEBS implements AutoBrakeSystem {
    *
    * @return the single instance of AEBS to be used.
    */
-  public AEBS aebs() {
+  @Override
+  public AEBS instance() {
     return instance;
   }
 
   /**
    * Returns the amount of brakes needed for the car.
    * Gives a 0 if no brake should be applied.
-   *
+   s*
    * @return returns the amount of brake value needed for the car
    */
-  @Override
   public double getBrakeValue() {
     return brakeValue;
   }
@@ -53,17 +53,13 @@ final class AEBS implements AutoBrakeSystem {
   /**
    * Evaluates the system's automatic braking system.
    * Based on the distance between the vehicle and the road hazards.
-   *
-   * @param distanceData the distance of objects from the car
-   * @param wheelSpeed the speed of the wheel.
    */
-  public void evaluateBraking(final double distanceData, final double wheelSpeed) {
-    // TODO: Check for any more parameters needed to be added
+  private void evaluateBraking() {
 
     // Determines whether or braking should happen or not
     // This is to save resources on the calculations
-    if (distanceData >= determineThreshold(wheelSpeed)) {
-      brakeValue = determineBrakes(distanceData, wheelSpeed);
+    if (distanceData >= determineThreshold()) {
+      brakeValue = determineBrakes();
     } else {
       brakeValue = 0;
     }
@@ -77,10 +73,9 @@ final class AEBS implements AutoBrakeSystem {
    * Calculates via:
    * Distance = Vehicle Speed * ReactionTime
    *
-   * @param wheelSpeed the speed of the wheel
    * @return the distance at which the vehicle should send brake
    */
-  private double determineThreshold(final double wheelSpeed) {
+  private double determineThreshold() {
     // the average human reaction time is 0.25 seconds
     // for this purpose we will account for those who have
     // slower reaction times by increasing this value
@@ -93,11 +88,9 @@ final class AEBS implements AutoBrakeSystem {
    * Determines how much the vehicle should brake.
    * Is calculated with the distance of other objects and vehicle wheel speed.
    *
-   * @param distanceData the distance of the objects from the car in km.
-   * @param wheelSpeed the speed of the wheel in km/h.
    * @return returns how much braking should be given
    */
-  private static double determineBrakes(final double distanceData, final double wheelSpeed) {
+  private static double determineBrakes() {
     double brakingPower = wheelSpeed - distanceData;
     return 0;
   }
@@ -106,10 +99,10 @@ final class AEBS implements AutoBrakeSystem {
    * Sends AEBS distance data of other obejcts on the road.
    * This should only be used by the Sensors.
    *
-   * @param distanceData the distance of the other objects from the vehicle.
+   * @param distanceDataReceived the distance of the other objects from the vehicle.
    */
-  public static void receiveDistanceAEBS(final double distanceData) {
-    distance = distanceData;
+  public static void receiveDistanceAEBS(final double distanceDataReceived) {
+    distanceData = distanceDataReceived;
     distanceReceived = true;
     instance.tick();
   }
@@ -118,19 +111,26 @@ final class AEBS implements AutoBrakeSystem {
    * Sends AEBS speed data of the vehicle.
    * This should only be used by the Sensors.
    *
-   * @param wheelSpeed the speed of the vehicle itself
+   * @param wheelSpeedReceived the speed of the vehicle itself
    */
-  public static void receiveSpeedAEBS(final double wheelSpeed) {
-    speed = wheelSpeed;
-    distanceReceived = true;
+  public static void receiveSpeedAEBS(final double wheelSpeedReceived) {
+    wheelSpeed = wheelSpeedReceived;
+    speedReceived = true;
     instance.tick();
   }
 
+  /**
+   * This ticks the AEBS to process the information that has been received.
+   * This is called by the methods receiveSpeedAEBS and
+   * receiveDistanceAEBS. Should only do something when
+   * both data has been gathered.
+   */
   private void tick() {
 
     // if the distance and speed has been received
     if (distanceReceived && speedReceived) {
 
+      evaluateBraking();
       distanceReceived = false;
       speedReceived = false;
     }

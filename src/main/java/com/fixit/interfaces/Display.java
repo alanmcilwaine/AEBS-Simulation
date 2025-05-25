de82fac680a332ea7ed3ea5f9ac668b9120f1726
Luigi Ibanez <ibanezluig@ecs.vuw.ac.nz>
2025-05-25 17:48:36 +1200

    created foreach loop that will check alerts

--- src/main/java/com/fixit/interfaces/Display.java
+++ src/main/java/com/fixit/interfaces/Display.java
@@ -1,5 +1,7 @@
 package com.fixit.interfaces;
 
+import java.util.ArrayList;
+
 /**
  * Display Class will store all the interfaces required to show
  * the user the relevant data that it needs to see.
@@ -9,6 +11,17 @@ class Display {
    *  speed variable is what will be used to display to driver.
    */
   private static double speed = 0;
+
+  /**
+   * UPPERBOUND will be used to limit the number of Alerts that
+   * can be stored.
+   */
+  public static final int UPPERBOUND = 99;
+  /**
+   * listOfAlerts stores all the alerts that need to be displayed
+   * to the driver.
+   */
+  private static ArrayList<String> listOfAlerts = new ArrayList<String>();
 
   /**
    * speedToShow is the putter method that updates the speed
@@ -27,6 +40,14 @@ class Display {
    */
   public static void errorsToShow(final String error) {
     //TODO: will add to list of errors to show to eventually display
+    int numAlerts = 0;
+    for (String alert : listOfAlerts) {
+      int maxAlerts = 99;
+      if (numAlerts >= maxAlerts) {
+        break;
+      }
+      numAlerts++;
+    }
   }
 
   /**

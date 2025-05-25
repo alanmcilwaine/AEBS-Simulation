b1a1cf29ee36e48d43a135fed3c873c4f42207bc
Luigi Ibanez <ibanezluig@ecs.vuw.ac.nz>
2025-05-25 19:36:21 +1200

    fixed merge conflict

--- src/main/java/com/fixit/interfaces/UserInterface.java
+++ src/main/java/com/fixit/interfaces/UserInterface.java
@@ -1,10 +1,12 @@
 package com.fixit.interfaces;
 
+import static com.fixit.interfaces.Display.display;
+import static com.fixit.interfaces.Display.errorsToShow;
+import static com.fixit.interfaces.Display.speedToShow;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import static com.fixit.interfaces.Display.errorsToShow;
-import static com.fixit.interfaces.Display.speedToShow;
 
 /**
  * UI class holds functionality with the UI.
@@ -13,127 +15,97 @@ import static com.fixit.interfaces.Display.speedToShow;
  */
 public class UserInterface implements Interface, KeyListener {
   /**
-   * autoSystemToggle method calls controlSignal to
-   * enable or disable the Automatic Emergency Braking system.
-   * Returns void, but tells the user the state
-   * of the system (on or off)
+   * autoSystemToggle method calls controlSignal to enable or disable
+   * the Automatic Emergency Braking system. Returns void, but tells the
+   * user the state of the system (on or off)
    */
-<<<<<<< HEAD
   public void autoSystemToggle() {
     //TODO: call CS to toggle Automatic Emergency Brake System
-=======
-  public static void autoSystemToggle() {
-    //call CS to toggle Automatic Emergency Brake System
->>>>>>> origin
   }
+
   /**
-   * applyBrake method calls controlSignal to apply the amount
-   * of brakes specified by the driver.
-   * Returns void
+   * applyBrake method calls controlSignal to apply the amount of
+   * brakes specified by the driver. Returns void
    *
    * @param brakePower The amount of power to send to the brakes.
    */
-
-<<<<<<< HEAD
   public void applyBrake(final double brakePower) {
     //TODO: call CS to apply brake
-=======
-  public static void applyBrake(final double brakePower) {
-    //call CS to apply brake
->>>>>>> origin
   }
+
   /**
-   * applyThrottle method calls controlSignal to apply the amount
-   * of throttle specified by the driver to speed up.
+   * applyThrottle method calls controlSignal to apply the amount of
+   * throttle specified by the driver to speed up.
    * Returns void.
    *
    * @param throttlePower the amount of throttle to send to car.
    */
-
-<<<<<<< HEAD
   public void applyThrottle(final double throttlePower) {
     //TODO: call CS to apply throttle
-=======
-  public static void applyThrottle(final double throttlePower) {
-    //call CS to apply throttle
->>>>>>> origin
   }
 
   /**
-   * receiveSpeed method gets called by sensor
-   * package and then calls the Display class speedToShow to
-   * display the speed to user.
+   * receiveSpeed method gets called by sensor package and then calls
+   * the Display class speedToShow to display the speed
+   * to user.
    *
-   * @param speed is the unit of measurement (kph) that
-   *        will be displayed to the user.
+   * @param speed is the unit of measurement (kph) that will be
+   *             displayed to the user.
    */
-<<<<<<< HEAD
-
-  public void receiveSpeed(final double speed) {
-    speedToShow(speed);
-=======
   public static void receiveSpeed(final double speed) {
-    System.out.println(speed);    //placeholder logic to show user speed
->>>>>>> origin
+    speedToShow(speed);
   }
 
   /**
-   * receiveWarning method gets called by ControlSignal package
-   * when there is a warning that needs to be displayed to the driver.
-   * This can be anything such as ABS being triggered, AutoBrakeSystem
-   * being triggered, or enabled/disabled.
+   * receiveWarning method gets called by ControlSignal package when there
+   * is a warning that needs to be displayed to the driver. This can be anything
+   * such as ABS being triggered, AutoBrakeSystem being triggered,
+   * or enabled/disabled.
    *
    * @param warning is the warning object that gets passed from
-   *                ControlSignal that gets displayed to the user.
+   *               ControlSignal that gets displayed to the user.
    */
-<<<<<<< HEAD
-  public void receiveWarning(final String warning) {
+  public static void receiveWarning(final String warning) {
     errorsToShow(warning);
   }
 
   /**
-   * tick method is the method used to update
-   * the Visual User Interface.
+   * tick method is the method used to update the Visual User Interface.
    */
   static void tick() {
-    Display.display();
+    display();
   }
 
   /**
-   * Invoked when a key has been typed. See the class description
-   * for {@link KeyEvent} for a definition of a key typed event.
+   * Invoked when a key has been typed. See the class description for
+   * {@link KeyEvent} for a definition of a key typed event.
    *
    * @param e the event to be processed
    */
   @Override
-  public void keyTyped(KeyEvent e) {
+  public void keyTyped(final KeyEvent e) {
 
   }
 
   /**
-   * Invoked when a key has been pressed. See the class description
-   * for {@link KeyEvent} for a definition of a key pressed event.
+   * Invoked when a key has been pressed. See the class description for
+   * {@link KeyEvent} for a definition of a key pressed event.
    *
    * @param e the event to be processed
    */
   @Override
-  public void keyPressed(KeyEvent e) {
+  public void keyPressed(final KeyEvent e) {
 
   }
 
   /**
-   * Invoked when a key has been released. See the class description
-   * for {@link KeyEvent} for a definition of a key released event.
+   * Invoked when a key has been released. See the class description for
+   * {@link KeyEvent} for a definition of a key released event.
    *
    * @param e the event to be processed
    */
   @Override
-  public void keyReleased(KeyEvent e) {
+  public void keyReleased(final KeyEvent e) {
 
-=======
-  public static void receiveWarning(final String warning) {
-    System.out.println(warning);
-    //placeholder logic to show user warning, not sure what data type yet.
->>>>>>> origin
   }
 }

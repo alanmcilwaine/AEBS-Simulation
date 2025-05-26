ed632d0f3fe53aecf1f649a0c15b640c71a95630
Luigi Ibanez <ibanezluig@ecs.vuw.ac.nz>
2025-05-26 22:59:04 +1200

    added test 3

--- src/test/java/fixit/simulation/R13Test.java
+++ src/test/java/fixit/simulation/R13Test.java
@@ -111,5 +111,27 @@ public class R13Test {
       assertFalse(printedOutput.contains("Alert Appearing sound"), "Expected no output, received " + printedOutput);
     }
   }
+  @Test
+  public void T03() {
+    outContent.reset();
+    Display.flushErrors();
 
+    Car car = Car.instance();
+    List<List<Double>> goodInputs = List.of(
+            List.of(10.0, 10.0, 50.0),
+            List.of(10.0, 50.0, 10.0),
+            List.of(100.0, 10.0, 10.0)
+    );
+    car.testRemoveLidar(0);
+
+    for (List<Double> goodInput : goodInputs) {
+      car.sensorInput(SensorType.LIDARLEFT, goodInput.get(0), Weather.SUNNY);
+      car.sensorInput(SensorType.LIDARCENTRE, goodInput.get(1), Weather.SUNNY);
+      car.sensorInput(SensorType.LIDARRIGHT, goodInput.get(2), Weather.SUNNY);
+
+      String printedOutput = outContent.toString();
+
+      assertFalse(printedOutput.contains("Alert Appearing sound"), "Expected no output, received " + printedOutput);
+    }
+  }
 }

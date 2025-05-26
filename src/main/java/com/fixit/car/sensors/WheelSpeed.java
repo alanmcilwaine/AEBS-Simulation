90c405b168b2e9e1339a7eafd505b547f2fa64fa
warrjac <warrjac@ecs.vuw.ac.nz>
2025-05-26 20:18:22 +1200

    Add javadoc

--- src/main/java/com/fixit/car/sensors/WheelSpeed.java
+++ src/main/java/com/fixit/car/sensors/WheelSpeed.java
@@ -5,24 +5,53 @@ import com.fixit.controlsignal.ControlSignals;
 import com.fixit.interfaces.UserInterface;
 import com.fixit.simulation.Weather;
 
-/** */
+/** 
+ * Sensor for sending wheel
+ * speed data to other packages
+*/
 public class WheelSpeed implements SpeedSensor {
-
+  /**
+   * Sends speed information to ControlSignal package
+   * 
+   * @param sensor An enum representing the sensors type
+   * @param data speed value to be sent (kmh)
+   */
   @Override
   public void sendToControlSignal(final SensorType sensor, final Double data) {
     ControlSignals.cs().processSensorData(sensor, data);
   }
 
+  /**
+   * Sends speed information to Interface package
+   * 
+   * @param data speed value to be sent (kmh)
+   */
   @Override
   public void sendToInterface(final Double data) {
     UserInterface.receiveSpeed(data);
   }
 
+  /**
+   * Sends speed information to AEBS package
+   * 
+   * @param sensor An enum representing the sensors type
+   * @param data speed value to be sent (kmh)
+   * @param weather An enum representing weather status
+   */
   @Override
   public void sendToAEBS(final SensorType sensor, final Double data, final Weather weather) {
     Aebs.instance().receiveSpeedAebs(data);
   }
 
+  /**
+   * Facilitates data distribution and
+   * handles data conversion from rpm to kmh
+   * 
+   * @param sensor An enum representing the sensors type
+   * @param data Value to be handled and sent (rpm)
+   * @param weather An enum representing weather status
+   * @return 1 if successful
+   */
   @Override
   public int readData(final SensorType sensor, final Double data, final Weather weather) {
     // Convert data (rpm) to dataKmh (kmh)

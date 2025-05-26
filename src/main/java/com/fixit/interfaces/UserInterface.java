a8be87b01b75f83beaed762fc22085c271830e96
Luigi Ibanez <ibanezluig@ecs.vuw.ac.nz>
2025-05-26 19:42:05 +1200

    removed code for User Interface to apply brakes to car, tidied checkstyle

--- src/main/java/com/fixit/interfaces/UserInterface.java
+++ src/main/java/com/fixit/interfaces/UserInterface.java
@@ -1,11 +1,14 @@
 package com.fixit.interfaces;
 
+import static com.fixit.interfaces.Display.display;
+import static com.fixit.interfaces.Display.errorsToRemove;
+import static com.fixit.interfaces.Display.errorsToShow;
+import static com.fixit.interfaces.Display.speedToShow;
+
 import com.fixit.aebs.Aebs;
 import com.fixit.aebs.Sensitivity;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
-import static com.fixit.interfaces.Display.*;
 
 
 /**
@@ -14,46 +17,6 @@ import static com.fixit.interfaces.Display.*;
  * disable the Automatic Braking system
  */
 public class UserInterface implements Interface, KeyListener {
-  /**
-   * throttlePower is the variable that keeps the value of how
-   * much throttle to apply.
-   */
-  private static double throttle = 0;
-
-  /**
-   * brakePower is the variable that keeps the value of how
-   * much throttle to apply.
-   */
-  private static double brake = 0;
-
-  /**
-   * INCREMENT is the value stored for applying more
-   * or less brakes or throttle.
-   */
-  private static final double INCREMENT = 0.1;
-
-  /**
-   * applyBrake method calls controlSignal to apply the amount of
-   * throttle specified by the driver to speed up.
-   * Returns void.
-   *
-   * @param brakePower the amount of brake to send to car.
-   */
-  public static void applyBrake(final double brakePower) {
-    //TODO: call CS to apply brake
-  }
-
-  /**
-   * applyThrottle method calls controlSignal to apply the amount of
-   * throttle specified by the driver to speed up.
-   * Returns void.
-   *
-   * @param throttlePower the amount of throttle to send to car.
-   */
-  public void applyThrottle(final double throttlePower) {
-    //TODO: call CS to apply throttle
-  }
-
   /**
    * receiveSpeed method gets called by sensor package and then calls
    * the Display class speedToShow to display the speed
@@ -131,30 +94,6 @@ public class UserInterface implements Interface, KeyListener {
     if (e.getKeyCode() == KeyEvent.VK_3) {
       Aebs.instance().setSensitivity(Sensitivity.HIGH);
       receiveWarning(" AEBS set to High ");
-    }
-    if (e.getKeyCode() == KeyEvent.VK_UP) {
-      assert throttle <= 1;
-      assert throttle >= 0;
-      throttle += INCREMENT;
-      applyThrottle(throttle);
-    }
-    if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-      assert throttle <= 1;
-      assert throttle >= 0;
-      throttle -= INCREMENT;
-      applyThrottle(throttle);
-    }
-    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-      assert brake <= 1;
-      assert brake >= 0;
-      brake += INCREMENT;
-      applyBrake(brake);
-    }
-    if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-      assert brake <= 1;
-      assert brake >= 0;
-      brake -= INCREMENT;
-      applyBrake(brake);
     }
   }
 

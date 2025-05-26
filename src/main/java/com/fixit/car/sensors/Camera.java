b349659de2a54bd89fe957e45a17c2e8c01bdf8a
warrjac <warrjac@ecs.vuw.ac.nz>
2025-05-26 20:13:01 +1200

    Adhere to 80 character line limit

--- src/main/java/com/fixit/car/sensors/Camera.java
+++ src/main/java/com/fixit/car/sensors/Camera.java
@@ -6,12 +6,16 @@ import com.fixit.simulation.Weather;
 public class Camera implements DistanceSensor {
 
   @Override
-  public void sendToAEBS(final SensorType sensor, final Double data, final Weather weather) {
+  public void sendToAEBS(final SensorType sensor, 
+                         final Double data, 
+                         final Weather weather) {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
-  public int readData(final SensorType sensor, final Double data, final Weather weather) {
+  public int readData(final SensorType sensor, 
+                      final Double data, 
+                      final Weather weather) {
     sendToAEBS(sensor, data, weather);
     return 1;
   }

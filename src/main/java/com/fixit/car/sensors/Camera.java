b43caa5c5b8f795ac51b3631445fc04d64976adf
warrjac <warrjac@ecs.vuw.ac.nz>
2025-05-26 20:17:31 +1200

    Add javadoc

--- src/main/java/com/fixit/car/sensors/Camera.java
+++ src/main/java/com/fixit/car/sensors/Camera.java
@@ -2,9 +2,22 @@ package com.fixit.car.sensors;
 
 import com.fixit.simulation.Weather;
 
-/** */
+/** 
+ * Unsupported class.
+ * 
+ * Sensor for sending camera
+ * distance data to other packages
+*/
 public class Camera implements DistanceSensor {
-
+  /**
+   * Unsupported method.
+   * 
+   * Sends distance information to AEBS package
+   * 
+   * @param sensor An enum representing the sensors type
+   * @param data distance value to be sent
+   * @param weather An enum representing weather status
+   */
   @Override
   public void sendToAEBS(final SensorType sensor, 
                          final Double data, 
@@ -12,6 +25,15 @@ public class Camera implements DistanceSensor {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
+  /**
+   * Facilitates distance data 
+   * distribution with sendToAEBS()
+   * 
+   * @param sensor An enum representing the sensors type
+   * @param data Distance value to be sent
+   * @param weather An enum representing weather status
+   * @return 1 if successful
+   */
   @Override
   public int readData(final SensorType sensor, 
                       final Double data, 
